f5c144cbbcbe6a085c44116341ad752f92384f1b
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-03-31 01:26:18 -0300

    add svg background update color s write new about

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,39 @@
 
-const bodyElem = document.querySelector("html");
-const contactElem = document.getElementsByClassName("footer-container")
+/* const bodyElem = document.querySelector("html");
+const contactElem = document.querySelector(".footer");
 
-document.getElementById('scroll').addEventListener('click', function () {
-  bodyElem.scrollIntoView(false);
-  // contactElem.classList.add("visible");
-});
+document.getElementById('scroll').addEventListener('click', () => {
+  contactElem.scrollIntoView(true)
+}); */
 
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.navbar__links');
+const navLinksA = document.querySelectorAll('.navbar__links a');
+
+const toggle = () => {
+  burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active')
+    burger.classList.toggle('toggle')
+
+    navLinksA.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+      }
+    })
+  })
+}
+
+toggle();
+
+
+const sectionOneImg = document.querySelector('.section__1');
+
+const parralax = () => {
+  window.addEventListener('scroll', () => {
+    sectionOneImg.style.backgroundPositionY = `${window.scrollY / 10}px`;
+  })
+}
+
+parralax()
