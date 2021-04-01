519033052f9e61b855913b45ba85a6fa3a6a638f
Altus Janse Van Rensburg <69439444+AltusJVR@users.noreply.github.com>
2021-03-31 21:49:51 -0300

    Update 30 march 2021 (#1)
    * add svg background update color s write new about
    
    * add helper classes to colors
    
    * update scroll offset, update img selecctor
    
    * update footer height, add footer border
    
    * update section height
    
    * add cards to home
    
    * add cards component
    
    * add project and home cards
    
    * add notes.md to .gitignore
    
    * update card hover
    
    * update card hover, link hover, overlay clr
    
    * update link hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,40 @@
 
-const bodyElem = document.querySelector("html");
-const contactElem = document.getElementsByClassName("footer-container")
+/* const bodyElem = document.querySelector("html");
+const contactElem = document.querySelector(".footer");
 
-document.getElementById('scroll').addEventListener('click', function () {
-  bodyElem.scrollIntoView(false);
-  // contactElem.classList.add("visible");
-});
+document.getElementById('scroll').addEventListener('click', () => {
+  contactElem.scrollIntoView(true)
+}); */
 
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.navbar__links');
+const navLinksA = document.querySelectorAll('.navbar__links a');
+
+const toggle = () => {
+  burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active')
+    burger.classList.toggle('toggle')
+
+    navLinksA.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+      }
+    })
+  })
+}
+
+toggle();
+
+
+const sectionImg = document.querySelector('.section');
+
+const parralax = () => {
+  console.log(window.scrollY)
+  window.addEventListener('scroll', () => {
+    sectionImg.style.backgroundPositionY = `${window.scrollY * 0.05}px`;
+  })
+}
+
+parralax()
