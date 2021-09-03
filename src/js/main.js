752c5fda9344a1efaf94c4fd36ef382ed873c3b7
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-09-03 18:02:42 -0300

    add event listener to toggle animation

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,21 @@ document.getElementById('scroll').addEventListener('click', () => {
   contactElem.scrollIntoView(true)
 }); */
 
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".navbar__links");
-const navLinksA = document.querySelectorAll(".navbar__links a");
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.navbar__links');
+const navLinksA = document.querySelectorAll('.navbar__links a');
+const shapes = document.querySelectorAll('.shape');
+const disableAnimationBtn = document.querySelector('.disable-animation');
 
 const toggle = () => {
-  burger.addEventListener("click", () => {
-    navLinks.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
+  burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
 
     navLinksA.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = "";
-        contact.style.clipPath = "";
+        link.style.animation = '';
+        contact.style.clipPath = '';
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${
           index / 7 + 0.3
@@ -29,20 +31,20 @@ const toggle = () => {
 
 const keyToggle = () => {
   let toggle = 0;
-  burger.addEventListener("keydown", (e) => {
+  burger.addEventListener('keydown', e => {
     if (!e.key === Tab) {
-      navLinks.classList.toggle("nav-active");
-      burger.classList.toggle("toggle");
+      navLinks.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
       navLinksA.forEach((link, index) => {
         if (link.style.animation) {
-          link.style.animation = "";
-          contact.style.clipPath = "";
-          link.setAttribute("tabindex", -1);
+          link.style.animation = '';
+          contact.style.clipPath = '';
+          link.setAttribute('tabindex', -1);
         } else {
           link.style.animation = `navLinkFade 0.5s ease forwards ${
             index / 7 + 0.3
           }s`;
-          link.setAttribute("tabindex", 0);
+          link.setAttribute('tabindex', 0);
         }
       });
     }
@@ -52,50 +54,70 @@ const keyToggle = () => {
 toggle();
 keyToggle();
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 const parralax = () => {
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     container.style.backgroundPositionY = `${window.scrollY * 0.1}px`;
   });
 };
 
 parralax();
 
-const contact = document.querySelector(".section__contact");
-const contactNavlink = document.querySelector("#scroll");
-const contactBurger = document.querySelector(".contact__burger");
+const contact = document.querySelector('.section__contact');
+const contactNavlink = document.querySelector('#scroll');
+const contactBurger = document.querySelector('.contact__burger');
 
 const showContact = () => {
-  contactNavlink.addEventListener("click", () => {
+  contactNavlink.addEventListener('click', () => {
     if (!contact.style.clipPath) {
-      contact.style.clipPath = "circle(75% at 50% 50%)";
+      contact.style.clipPath = 'circle(75% at 50% 50%)';
     } else {
-      contact.style.clipPath = "";
+      contact.style.clipPath = '';
     }
   });
 };
 
 const closeContact = () => {
-  contactBurger.addEventListener("click", () => {
+  contactBurger.addEventListener('click', () => {
     if (!contact.style.clipPath) {
-      contact.style.clipPath = "circle(75% at 50% 50%)";
+      contact.style.clipPath = 'circle(75% at 50% 50%)';
     } else {
-      contact.style.clipPath = "";
+      contact.style.clipPath = '';
     }
   });
 };
 
 const keyCloseContact = () => {
-  contactBurger.addEventListener("keydown", (e) => {
+  contactBurger.addEventListener('keydown', e => {
     e.preventDefault;
-    if (!contact.style.clipPath && !e.key === "Tab") {
-      contact.style.clipPath = "circle(75% at 50% 50%)";
+    if (!contact.style.clipPath && !e.key === 'Tab') {
+      contact.style.clipPath = 'circle(75% at 50% 50%)';
     } else {
-      contact.style.clipPath = "";
+      contact.style.clipPath = '';
     }
   });
 };
 keyCloseContact();
 closeContact();
-showContact();
+
+const shapeContainer = document.querySelector('.shapes');
+const toggleAnimation = () => {
+  disableAnimationBtn.addEventListener('click', () => {
+    shapeContainer.classList.toggle('animate');
+    animateDelay();
+  });
+};
+const animateDelay = () => {
+  if (shapeContainer.classList[1] === 'animate') {
+    shapes.forEach(shape => {
+      shape.style.animationDelay = `${Math.floor(Math.random() * 4000)}ms`;
+    });
+  } else {
+    shapes.forEach(shape => {
+      shape.style.animationDelay = '';
+    });
+  }
+};
+animateDelay();
+toggleAnimation();
